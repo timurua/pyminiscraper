c7d1baba81f7f5a17ec8cfb1bbb7afdf3fdd73af
timur <timurua@gmail.com>
2025-01-06 14:39:25 -0800

    Fixing project name in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 setup(
-    name="pyminiscaper",
+    name="pyminiscraper",
     version="0.1.1",
     packages=find_packages(),
     install_requires=[
